4048b7f971cf9f37a791103434c2724855a5d412
--local <kalrajanvi99@gmail.com>
2023-01-15 21:42:14 -0800

    add prettier

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,28 @@
-import Head from 'next/head';
-import Image from 'next/image';
-import buildspaceLogo from '../assets/buildspace-logo.png';
-import { useState } from 'react';
+import Head from "next/head";
+import Image from "next/image";
+import buildspaceLogo from "../assets/buildspace-logo.png";
+import { useState } from "react";
 
 const Home = () => {
-  const [userInput, setUserInput] = useState('');
+  const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  const [isGenerating, setIsGenerating] = useState(false);
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+    const response = await fetch("/api/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ userInput }),
+    });
+    const { output } = await response.json();
+    setApiOutput(output);
+
+    console.log(apiOutput);
+    setIsGenerating(false);
+  };
 
   return (
     <div className="root">
@@ -18,31 +36,32 @@ const Home = () => {
           </div>
         </div>
         <div className="prompt-container">
-          <textarea 
+          <textarea
             value={userInput}
             onChange={(e) => setUserInput(e.target.value)}
             placeholder="start typing here"
-            className="prompt-box" />
+            className="prompt-box"
+          />
         </div>
         <div className="prompt-buttons">
-          <a className="generate-button" onClick={null}>
+          <a className="generate-button" onClick={callGenerateEndpoint}>
             <div className="generate">
               <p>Generate</p>
             </div>
           </a>
         </div>
-      </div>
-      <div className="badge-container grow">
-        <a
-          href="https://buildspace.so/builds/ai-writer"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="badge">
-            <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+        {apiOutput && (
+          <div className="output">
+            <div className="output-header-container">
+              <div className="output-header">
+                <h3>Output</h3>
+              </div>
+            </div>
+            <div className="output-content">
+              <p>{apiOutput}</p>
+            </div>
           </div>
-        </a>
+        )}
       </div>
     </div>
   );
